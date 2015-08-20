580cb6d92da3fb00af05d9a28cea8c3958a959a0
HaiBin <haibinmath@gmail.com>
2015-08-20 11:16:43 -0400

    Edit
    Edit

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # Cleaning-Data-Project
 
-# Hello
+The R code does the following:
 
-This is a new markdown file.
+## Extract info from the "test" folder
+
+### Merging info from the "test" folder
+
+## Extract info from the "train" folder
+
+### Merging info from the "train" folder
+
+## Merging the "test" and "train" sets together.
+
+## Read all the column names
+
+### Manually input the names for the "activity" and "subject" column
+
+## Locate all columns with names including the phrase "mean" or "std", but exclude the phrease "meanFreq"
+
+### Manually include the "activity" and "subject" column
+
+## Extract our required data
+
+## Converting a numeric vector to a factor vector
+
+### Manually assigning the levels for this factor vector
+
+## Assigning the column names
+
+## Finding the mean of each observation, grouped by "activity" and "subject"
+
+## Reassigning the column name, because we are finding the mean of the mean, and mean of the deviation.
+
+### Manually reassign the "activity" and "subject" column name.
+
+## Write final data into a txt file.
--- run_analysis.R
+++ run_analysis.R
@@ -1,39 +1,66 @@
 project<-function(){
+    ### Extract info from "test" sets
     TESTSET<-read.table("./UCI HAR Dataset/test/X_test.txt")
     TESTLABEL<-read.table("./UCI HAR Dataset/test/y_test.txt")
     TESTWINDOW<-read.table("./UCI HAR Dataset/test/subject_test.txt")
-    ######
+    ### Merging all info from "test" sets
+    COMBINEDTEST<-cbind(TESTSET,TESTLABEL,TESTWINDOW)
+    
+    ### Extract info from "train" sets
     TRAININGSET<-read.table("./UCI HAR Dataset/train/X_train.txt")
     TRAININGLABEL<-read.table("./UCI HAR Dataset/train/y_train.txt")
     TRAININGWINDOW<-read.table("./UCI HAR Dataset/train/subject_train.txt")
-    ### Create merged set
-    COMBINEDTEST<-cbind(TESTSET,TESTLABEL,TESTWINDOW)
+    ### Merging all info from "train" sets
     COMBINEDTRAINING<-cbind(TRAININGSET,TRAININGLABEL,TRAININGWINDOW)
+    
+    ### Merge the "test" and "train" sets together.
     NEWSET<-rbind(COMBINEDTEST,COMBINEDTRAINING)
-    ###
+    
+    ### Read all the column names
     NAMES<-read.table("./UCI HAR Dataset/features.txt")[,2]
+    
+    ### Manually input the names for the "activity" and "subject" column
     levels(NAMES)<-c(levels(NAMES),"activity","subject")
     NAMES[length(NAMES)+1]<-"activity"
     NAMES[length(NAMES)+1]<-"subject"
-    MEAN_OR_STD<-grepl("mean|std",NAMES)
+    
+    ### Locate all columns with names including the phrase "mean" or "std"
+    ### But exclude the phrease "meanFreq".
+    INCLUDE<-grepl("mean|std",NAMES)
     EXCLUDE<-grepl("meanFreq",NAMES)
-    MEAN_OR_STD<-MEAN_OR_STD & !EXCLUDE
+    MEAN_OR_STD<-INCLUDE & !EXCLUDE
+    
+    ### Manually include the "activity" and "subject" column
     MEAN_OR_STD[c(length(NAMES)-1,length(NAMES))]<-TRUE
-    ###
+    
+    ### Extract our required data
     EXTRACTED_DATA<-NEWSET[,MEAN_OR_STD]
-    ###
+    
+    ### Converting a numeric vector to a factor vector
     NUM_COL<-ncol(EXTRACTED_DATA)
     EXTRACTED_DATA[,NUM_COL-1]<-factor(EXTRACTED_DATA[,NUM_COL-1])
-    levels(EXTRACTED_DATA[,NUM_COL-1])<-c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS"
+    
+    ### Manually assigning the levels for this factor vector
+    levels(EXTRACTED_DATA[,NUM_COL-1])<-c("WALKING","WALKING UPSTAIRS","WALKING DOWNSTAIRS"
                                           ,"SITTING","STANDING","LAYING")
-    ###
+    
+    ### Assigning the column names
     EXTRACTED_NAMES<-NAMES[MEAN_OR_STD]
     names(EXTRACTED_DATA)<-EXTRACTED_NAMES
+    
+    ### Finding the mean of each observation, grouped by "activity" and "subject"
     NEW_DATA<-aggregate(EXTRACTED_DATA[,1:(NUM_COL-2)],
                         by=list(EXTRACTED_DATA$activity,
                                 EXTRACTED_DATA$subject),mean)
+    
+    ### Reassigning the column name, because we are finding the mean of the mean, 
+    ### and mean of the deviation.
     names(NEW_DATA)<-paste("MEAN[",names(NEW_DATA),"]",sep="")
+    
+    ### Manually reassign the "activity" and "subject" column name.
     names(NEW_DATA)[1]<-"activity"
     names(NEW_DATA)[2]<-"subject"
+    
+    ### Write final data into a txt file.
     write.table(NEW_DATA,row.name=FALSE,file="./projectsubmit.txt")
 }
